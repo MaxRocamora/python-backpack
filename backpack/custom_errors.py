0cadb8855882093813372149f3e5ec2861862325
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-19 14:53:09 -0300

    added errors

--- backpack/custom_errors.py
+++ backpack/custom_errors.py
@@ -5,12 +5,13 @@
 # ----------------------------------------------------------------------------------------
 
 class EnvironmentVariableNotFound(Exception):
-    '''Raise when a required environment variable os not found.
-    Args:
-        var_name (str) name of the required variable missing
-    '''
 
-    def __init__(self, var_name):
+    def __init__(self, var_name: str) -> None:
+        '''Error Raised when a required environment variable is missing from os.
+
+        Args:
+            var_name (str): name of the required variable missing
+        '''
         self.var_name = var_name
         self.message = f'System required ({var_name}) Environment Variable not found.'
         super().__init__(self.message)
@@ -20,11 +21,15 @@ class EnvironmentVariableNotFound(Exception):
 
 
 class ApplicationNotFound(Exception):
-    '''Raise when a background application is not found.'''
 
-    def __init__(self, var_name):
-        self.var_name = var_name
-        self.message = f'Application ({var_name}) not found.'
+    def __init__(self, app_name: str) -> None:
+        '''Error Raised when an Application Name required is not found.
+
+        Args:
+            app_name (str): name of the required application missing
+        '''
+        self.app_name = app_name
+        self.message = f'Application ({app_name}) not found.'
         super().__init__(self.message)
 
     def __str__(self):
