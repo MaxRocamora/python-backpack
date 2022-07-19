213960febf928d312dccaf93a0a9462937821d1b
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-19 18:26:18 -0300

    Update file_utils.py

---
+++ backpack/file_utils.py
@@ -0,0 +1,5 @@
+# ----------------------------------------------------------------------------------------
+# Python-Backpack - File Utilities
+# Maximiliano Rocamora / maxirocamora@gmail.com
+# https://github.com/MaxRocamora/python-backpack
+# ----------------------------------------------------------------------------------------
