ab2dd9094b5da222c2e7eb8084ab8c389b5d2373
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-19 21:14:28 -0300

    Delete file_utils.py

--- backpack/file_utils.py
+++
@@ -1,5 +0,0 @@
-# ----------------------------------------------------------------------------------------
-# Python-Backpack - File Utilities
-# Maximiliano Rocamora / maxirocamora@gmail.com
-# https://github.com/MaxRocamora/python-backpack
-# ----------------------------------------------------------------------------------------
