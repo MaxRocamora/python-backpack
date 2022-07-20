2ba154b466c7337aa80f5effb87a48d0dee48baa
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-19 23:13:08 -0300

    refactor methods

--- backpack/file_utils.py
+++ backpack/file_utils.py
@@ -4,7 +4,6 @@
 # https://github.com/MaxRocamora/python-backpack
 # ----------------------------------------------------------------------------------------
 
-import os
 import contextlib
 
 from backpack.logger import get_logger
@@ -14,37 +13,26 @@ log = get_logger('FileUtils')
 
 def replace_strings_in_file(ascii_file: str, strings: list, new_string: str):
     ''' Opens ascii file and replaces all occurrences from strings into new_string.
-    In this class we use a full path to avoid use of os.dirname, dirname, which
+    In this class we use a full path to avoid use of os.dirname, which
     causes string encode problems.
     Args:
         ascii_file (fullpath) file to open
         strings (list) strings to replace
         new_string (string) new string or path to set
     '''
-    if not strings:
-        return False
 
-    log.process('Replacing Strings...')
-
+    # force forward slashes
     new_string = new_string.replace("\\", "/")
 
-    log.info("Opening File: %s", ascii_file)
+    log.info(f"Replacing Strings, Opening File: {ascii_file}")
 
     with open(ascii_file) as f:
         file_data = f.read()
         for i in strings:
-            source = i.replace("\\", "/")
-            filename = os.path.split(source)[1]
-            target = os.path.join(new_string, str(filename))
-            target = target.replace("\\", "/")
-            log.info('Finding: %s', source)
-            log.info('Replacing for: %s', target)
+            log.info('Finding: %s', i)
+            log.info('Replacing for: %s', new_string)
             log.info('-' * 50)
-            try:
-                file_data = file_data.replace(source, target)
-            except UnicodeDecodeError as e:
-                log.info('Skipping RePath due to this error.')
-                log.info('UnicodeDecodeError: %s', str(e))
+            file_data = file_data.replace(i, new_string)
 
     with open(ascii_file, 'w') as f:
         f.write(file_data)
@@ -52,24 +40,22 @@ def replace_strings_in_file(ascii_file: str, strings: list, new_string: str):
         log.info(f"Closing File: {ascii_file}")
 
 
-def remove_strings_from_file(ascii_file: str, strings: str):
-    '''
-    removes given string list from to file.
+def remove_line_from_file(ascii_file: str, strings: str):
+    ''' removes given lines from ascii file.
     Args:
-        ascii_file (path) ASCII File to process
-        strings (path) List of lines to remove
+        ascii_file (path) ASCII file to process
+        strings (list) match lines to remove
     '''
-    with open(strings) as fileData:
-        stringList = fileData.read().splitlines()
 
-    fileData = None
-    with open(ascii_file) as fileData:
-        fileData = fileData.read().splitlines()
+    with open(ascii_file) as f:
+        file_content = f.read().splitlines()
 
-    for line in stringList:
+    for line in file_content:
+        print('file line', line)
         with contextlib.suppress(ValueError):
-            if line in fileData:
-                fileData.pop(fileData.index(line))
+            if line in strings:
+                file_content.pop(file_content.index(line))
+
     with open(ascii_file, "w") as f:
-        contents = "\n".join(fileData)
+        contents = "\n".join(file_content)
         f.write(contents)
