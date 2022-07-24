4d1ebf8b7cfe79587cc40f4ff1c434e1c5b8c4fc
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-24 15:27:36 -0300

    Update test_json_utils.py

--- tests/test_json_utils.py
+++ tests/test_json_utils.py
@@ -15,17 +15,16 @@ mod_path = os.path.dirname(__file__)
 if mod_path not in sys.path:
     sys.path.append(mod_path)
 
-NO_FILE = os.path.join(mod_path, 'test_files', 'no_file.json')
-JSON_FILE = os.path.join(mod_path, 'test_files', 'data.json')
-JSON_SAVE_FILE = os.path.join(mod_path, 'test_files', 'save', 'data_saved.json')
-BROKEN_JSON_FILE = os.path.join(mod_path, 'test_files', 'data_broken.json')
+NO_FILE = os.path.join(mod_path, 'test_json', 'no_file.json')
+JSON_LOAD_FILE = os.path.join(mod_path, 'test_json', 'data.json')
+JSON_SAVE_FILE = os.path.join(mod_path, 'test_json', 'save', 'data_saved.json')
+BROKEN_JSON_FILE = os.path.join(mod_path, 'test_json', 'data_broken.json')
 
 
 class Test_Errors(unittest.TestCase):
 
-    def test_json_open(self):
-        ''' testing module '''
-        data = json_load(JSON_FILE)
+    def test_json_load(self):
+        data = json_load(JSON_LOAD_FILE)
         self.assertTrue(type(data), dict)
         self.assertTrue(data.get('user'), 'Max')
         self.assertRaises(OSError, json_load, NO_FILE)
@@ -35,8 +34,12 @@ class Test_Errors(unittest.TestCase):
         data = {'name': 'max'}
         r = json_save(data, JSON_SAVE_FILE)
         self.assertTrue(r)
+        # remove files
         remove_files_in_dir(os.path.dirname(JSON_SAVE_FILE))
         os.removedirs(os.path.dirname(JSON_SAVE_FILE))
+
+        # force error
+        self.assertFalse(json_save(unittest, JSON_SAVE_FILE))
 
 
 if __name__ == '__main__':
