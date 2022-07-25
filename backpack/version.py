89aca7b3d30478e17ff7cd3dc514aa9677ef503f
Maximilliano Rocamora <maxirocamora@gmail.com>
2022-07-25 17:38:53 -0300

    1.0.3

--- backpack/version.py
+++ backpack/version.py
@@ -1,11 +1,12 @@
 # ----------------------------------------------------------------------------------------
 # 1.0.0 07/2022 - Initial Release
 # 1.0.2 07/2022 - More Methods and tests
+# 1.0.3 07/2022 - JsonMetaFile, JsonUserSettings
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_PATCH = 2
+VERSION_PATCH = 3
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
