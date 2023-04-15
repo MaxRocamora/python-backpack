6770ba20a559875c8bf8cfaff0f2fa514c0a59e0
Maximilliano Rocamora <maxirocamora@gmail.com>
2023-04-14 23:37:39 -0300

    camelcase_to_snakecase method tests

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -9,7 +9,7 @@ import sys
 import unittest
 
 from backpack.strings import has_numbers, begin_with_number, begin_or_end_with_numbers
-from backpack.strings import reformat_input_string
+from backpack.strings import reformat_input_string, camelcase_to_snakecase
 
 
 mod_path = os.path.dirname(__file__)
@@ -66,6 +66,28 @@ class Test_Errors(unittest.TestCase):
             reformat_input_string(
                 REFORMAT_BOTH, under_spaces=False, under_hyphen=False), REFORMAT_BOTH
         )
+
+    def test_camelcase_to_snakecase(self):
+
+        # Test case 1: Input with no uppercase characters
+        input_str, expected_output = "nocamelcase", "nocamelcase"
+        self.assertEqual(camelcase_to_snakecase(input_str), expected_output)
+
+        # Test case 2: Input with single uppercase character
+        input_str, expected_output = "OneCamelCaseChar", "one_camel_case_char"
+        self.assertEqual(camelcase_to_snakecase(input_str), expected_output)
+
+        # Test case 3: Input with multiple uppercase characters
+        input_str, expected_output = "MultiCamelCaseString", "multi_camel_case_string"
+        self.assertEqual(camelcase_to_snakecase(input_str), expected_output)
+
+        # Test case 4: Input with leading and trailing underscores
+        input_str, expected_output = "_CamelCase_", "_camel_case_"
+        self.assertEqual(camelcase_to_snakecase(input_str), expected_output)
+
+        # Test case 5: Input with leading and trailing underscores and lowercase
+        input_str, expected_output = "_camel_Case_", "_camel_case_"
+        self.assertEqual(camelcase_to_snakecase(input_str), expected_output)
 
 
 if __name__ == '__main__':
