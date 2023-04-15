8bbc26f3a4e8a8ebfd3614f5b382fb257b682542
Maximilliano Rocamora <maxirocamora@gmail.com>
2023-04-14 23:35:22 -0300

    python +3.7

--- setup.py
+++ setup.py
@@ -24,9 +24,7 @@ setup(
     license='GNU GENERAL PUBLIC LICENSE',
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python >= 3.7",
         "License :: Freely Distributable",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
