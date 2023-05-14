fa268f5a066260c1b25766cbb995b7cc2266a2c0
Maximilliano Rocamora <maxirocamora@gmail.com>
2023-05-14 19:49:05 -0300

    1.0.5

--- backpack/version.py
+++ backpack/version.py
@@ -3,11 +3,12 @@
 # 1.0.2 07/2022 - More Methods and tests
 # 1.0.3 07/2022 - JsonMetaFile, JsonUserSettings
 # 1.0.4 04/2023 - Added camelcase_to_snakecase function
+# 1.0.5 05/2023 - Refactor JsonMetaFile, Added Cache, Some type hints / docstrings
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 0
-VERSION_PATCH = 4
+VERSION_PATCH = 5
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
