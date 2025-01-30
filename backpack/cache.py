ccd2d77fd51262d8574cdca947dbce2771fcd7c8
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-01-30 14:29:11 -0300

    ruff formatting

--- backpack/cache.py
+++ backpack/cache.py
@@ -3,9 +3,8 @@
 # Maximiliano Rocamora / maxirocamora@gmail.com
 # https://github.com/MaxRocamora/python-backpack
 # ----------------------------------------------------------------------------------------
+from datetime import datetime, timedelta, timezone
 from functools import lru_cache, wraps
-from datetime import datetime, timedelta
-from datetime import timezone
 
 from backpack.logger import get_logger
 
@@ -13,7 +12,8 @@ log = get_logger('Python Backpack - Cache')
 
 
 def timed_lru_cache(seconds: int, maxsize: int = 128):
-    ''' lru_cache with expiration time
+    """Lru_cache with expiration time.
+
     Args:
         seconds (int): expiration time in seconds
         maxsize (int): maxsize for lru_cache
@@ -34,26 +34,27 @@ def timed_lru_cache(seconds: int, maxsize: int = 128):
         # * to clear the cache, use force_clear=True on the function call
         my_function(force_clear=True)
 
-    '''
+    """
 
     def wrapper_cache(func):
-
         func = lru_cache(maxsize=maxsize)(func)
         func.lifetime = timedelta(seconds=seconds)
         func.expiration = datetime.now(timezone.utc) + func.lifetime
 
         @wraps(func)
         def wrapped_func(*args, force_clear: bool = False, show_log: bool = False, **kwargs):
-            ''' wrapper function for lru_cache with expiration time
+            """Wrapper function for lru_cache with expiration time.
+
             Args:
+                *args: function arguments
                 force_clear (bool): forces a clear cache
                 show_log (bool): show log on clear
+                **kwargs: function keyword arguments
             Returns:
                 function result
-            '''
+            """
 
             if force_clear or datetime.now(timezone.utc) >= func.expiration:
-
                 if show_log:
                     log.debug(f'Cache cleared for {func.__name__}')
 
