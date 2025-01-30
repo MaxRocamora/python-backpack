482e700e78ab30be8732aeb77b4bc8de622a7526
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-01-30 14:29:57 -0300

    ruff formatting

--- backpack/folder_utils.py
+++ backpack/folder_utils.py
@@ -14,38 +14,39 @@ log = get_logger('Python Backpack - FolderUtils')
 
 
 def browse_folder(folder: str) -> bool:
-    ''' Open windows explorer on folder
+    """Open windows explorer on folder.
+
     Args:
-        folder (path) folder to open
-    '''
+        folder: (string path) folder to open
+    """
     if folder and os.path.isdir(folder):
-        subprocess.Popen(f"explorer {os.path.abspath(folder)}")
+        subprocess.Popen(f'explorer {os.path.abspath(folder)}')
         return True
 
-    log.warning(f"Unable to open folder {folder}")
+    log.warning(f'Unable to open folder {folder}')
     return False
 
 
 def create_folders(folders: list, force_empty: bool = False, verbose: bool = False):
-    ''' Creates multiple folders on disc.
+    """Creates multiple folders on disc.
 
     Args:
         folders (list): folder list
         force_empty (bool, optional): forces clearing folder content. Defaults to False.
         verbose (bool, optional):shows log. Defaults to False.
-    '''
+    """
     for folder in folders:
         create_folder(folder, force_empty=force_empty, verbose=verbose)
 
 
 def create_folder(path: str, force_empty: bool = False, verbose: bool = True):
-    '''creates a folder
+    """Creates a folder.
 
     Args:
         path (str): folder path
         force_empty (bool, optional): forces clearing folder content. Defaults to False.
         verbose (bool, optional): show log. Defaults to True.
-    '''
+    """
     abspath = os.path.abspath(path)
     if verbose:
         log.info(f'Creating Folder {abspath}')
@@ -56,14 +57,14 @@ def create_folder(path: str, force_empty: bool = False, verbose: bool = True):
         elif force_empty:
             remove_files_in_dir(abspath)
     except OSError as e:
-        log.warning(f"Unable to create folder: {path}")
+        log.warning(f'Unable to create folder: {path}')
         log.error(str(e))
 
     return True
 
 
 def remove_files_in_dir(path: str):
-    ''' clears all content in given directory '''
+    """Clears all content in given directory."""
     for root, dirs, files in os.walk(path):
         for f in files:
             os.unlink(os.path.join(root, f))
@@ -72,12 +73,12 @@ def remove_files_in_dir(path: str):
 
 
 def recursive_dir_copy(source_path: str, target_path: str):
-    ''' Copy all files src dir to dest dir, including sub-directories.
+    """Copy all files src dir to dest dir, including sub-directories.
 
     Args:
         source_path (str): source path
         target_path (str): destination path
-    '''
+    """
 
     create_folder(source_path)
     create_folder(target_path)
