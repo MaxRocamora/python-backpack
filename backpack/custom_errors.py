5bd363c87d1e8386f67b8029b9d4f55b6ffb047d
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-01-30 14:29:16 -0300

    ruff formatting

--- backpack/custom_errors.py
+++ backpack/custom_errors.py
@@ -4,33 +4,34 @@
 # https://github.com/MaxRocamora/python-backpack
 # ----------------------------------------------------------------------------------------
 
-class EnvironmentVariableNotFound(Exception):
 
+class EnvironmentVariableNotFound(Exception):
     def __init__(self, var_name: str) -> None:
-        '''Error Raised when a required environment variable is missing from os.
+        """Error Raised when a required environment variable is missing from os.
 
         Args:
             var_name (str): name of the required variable missing
-        '''
+        """
         self.var_name = var_name
         self.message = f'Required Environment Variable [{var_name}] not found.'
         super().__init__(self.message)
 
     def __str__(self):
+        """Return the error message."""
         return self.message
 
 
 class ApplicationNotFound(Exception):
-
     def __init__(self, app_name: str) -> None:
-        '''Error Raised when an Application Name required is not found.
+        """Error Raised when an Application Name required is not found.
 
         Args:
             app_name (str): name of the required application missing
-        '''
+        """
         self.app_name = app_name
         self.message = f'Application ({app_name}) not found.'
         super().__init__(self.message)
 
     def __str__(self):
+        """Return the error message."""
         return self.message
