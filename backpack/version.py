1e146316b57a6f9860532df538727605f782c662
Maximilliano Rocamora <maxirocamora@gmail.com>
2025-07-05 15:43:39 -0300

    update: increment patch version to 1.1.2 and update version history

--- backpack/version.py
+++ backpack/version.py
@@ -5,11 +5,13 @@
 # 1.0.4 04/2023 - Added camelcase_to_snakecase function
 # 1.0.5 05/2023 - Refactor JsonMetaFile, Added Cache, Some type hints / docstrings
 # 1.1.0 01/2024 - Ruff Formatting
+# 1.1.1 07/2025 - Update Python version matrix to include 3.10 and 3.11, update actions versions
+# 1.1.2 07/2025 - Update actions versions, remove unused badge from README
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
 VERSION_MINOR = 1
-VERSION_PATCH = 0
+VERSION_PATCH = 2
 
 version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
